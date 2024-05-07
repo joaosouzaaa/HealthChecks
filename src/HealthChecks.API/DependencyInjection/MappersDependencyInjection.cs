266ae1be11703545994cba286e5368b6475e4c94
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-07 19:16:06 -0300

    Feat: Adding ClientMapper.

--- src/HealthChecks.API/DependencyInjection/MappersDependencyInjection.cs
+++ src/HealthChecks.API/DependencyInjection/MappersDependencyInjection.cs
@@ -1,9 +1,12 @@
-﻿namespace HealthChecks.API.DependencyInjection;
+﻿using HealthChecks.API.Interfaces.Mappers;
+using HealthChecks.API.Mappers;
+
+namespace HealthChecks.API.DependencyInjection;
 
 internal static class MappersDependencyInjection
 {
     internal static void AddMappersDependencyInjection(this IServiceCollection services)
     {
-
+        services.AddScoped<IClientMapper, ClientMapper>();
     }
 }
---
+++ src/HealthChecks.API/Interfaces/Mappers/IClientMapper.cs
@@ -0,0 +1,10 @@
+﻿using HealthChecks.API.DataTransferObjects.Client;
+using HealthChecks.API.Entities;
+
+namespace HealthChecks.API.Interfaces.Mappers;
+
+public interface IClientMapper
+{
+    Client SaveToDomain(ClientSave clientSave);
+    List<ClientResponse> DomainListToResponseList(List<Client> clientList);
+}
