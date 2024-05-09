e385971b7325111315a1c271996ceb1a1ce3cc62
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-08 21:29:23 -0300

    Tests: ClientNotificationMaoper UnitTests.

---
+++ src/Tests/UnitTests/MappersTests/ClientNotificationMapperTests.cs
@@ -0,0 +1,33 @@
+﻿using HealthChecks.API.Entities;
+using HealthChecks.API.Mappers;
+using UnitTests.TestBuilders;
+
+namespace UnitTests.MappersTests;
+
+public sealed class ClientNotificationMapperTests
+{
+    private readonly ClientNotificationMapper _clientNotificationMapper;
+
+    public ClientNotificationMapperTests()
+    {
+        _clientNotificationMapper = new ClientNotificationMapper();
+    }
+
+    [Fact]
+    public void DomainListToResponseList_SuccessfulScenario_ReturnsResponseList()
+    {
+        // A
+        var clientNotificationList = new List<ClientNotification>()
+        {
+            ClientNotificationBuilder.NewObject().DomainBuild(),
+            ClientNotificationBuilder.NewObject().DomainBuild(),
+            ClientNotificationBuilder.NewObject().DomainBuild()
+        };
+
+        // A
+        var clientNotificationResponseListResult = _clientNotificationMapper.DomainListToResponseList(clientNotificationList);
+
+        // A
+        Assert.Equal(clientNotificationResponseListResult.Count, clientNotificationList.Count);
+    }
+}
