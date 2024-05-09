b7b2940c1b1f92ab509689e462c78f24f85ffdf1
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-08 21:31:45 -0300

    Feat: Adding ClientNotificationService.

--- src/HealthChecks.API/Interfaces/Services/IClientNotificationService.cs
+++ src/HealthChecks.API/Interfaces/Services/IClientNotificationService.cs
@@ -1,5 +1,8 @@
-﻿namespace HealthChecks.API.Interfaces.Services;
+﻿using HealthChecks.API.DataTransferObjects.ClientNotification;
+
+namespace HealthChecks.API.Interfaces.Services;
 
 public interface IClientNotificationService
 {
+    Task<List<ClientNotificationResponse>> GetAllByClientIdAsync(long clientId);
 }
--- src/HealthChecks.API/Services/ClientNotificationService.cs
+++ src/HealthChecks.API/Services/ClientNotificationService.cs
@@ -1,16 +1,19 @@
-﻿using HealthChecks.API.Interfaces.Repositories;
+﻿using HealthChecks.API.DataTransferObjects.ClientNotification;
+using HealthChecks.API.Interfaces.Mappers;
+using HealthChecks.API.Interfaces.Repositories;
 using HealthChecks.API.Interfaces.Services;
 
 namespace HealthChecks.API.Services;
 
 public sealed class ClientNotificationService(
-    IClientNotificationRepository clientNotificationRepository) 
+    IClientNotificationRepository clientNotificationRepository,
+    IClientNotificationMapper clientNotificationMapper) 
     : IClientNotificationService
 {
-    public async Task GetAllByClientIdAsync(long clientId)
+    public async Task<List<ClientNotificationResponse>> GetAllByClientIdAsync(long clientId)
     {
         var clientNotificationList = await clientNotificationRepository.GetAllByClientIdAsync(clientId);
 
-        
+        return clientNotificationMapper.DomainListToResponseList(clientNotificationList);
     }
 }
